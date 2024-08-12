c173f215e79ac76ebb8790590391aceb19291115
David Perl <david.perl@diamond.ac.uk>
2024-08-12 11:03:57 +0100

    add button widget

--- src/components/pv.tsx
+++ src/components/pv.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { Box, Text, HStack } from "@chakra-ui/react";
+import { Box, Text, HStack, Button } from "@chakra-ui/react";
 import { gql, useQuery } from "@apollo/client";
 
-const getPvQuery = (pv: string, type:string) => gql(`
+const getPvQuery = (pv: string, type: string) =>
+  gql(`
   query {
     getChannel(id: "${pv}") {
       value {
@@ -10,40 +11,67 @@ const getPvQuery = (pv: string, type:string) => gql(`
       }
     }
   }
-`)
+`);
 
-export const PollingRoPvBox = ({
-  name,
-  pv,
-  pollInterval = 100,
-  sigFigs = 4,
-  dType = "float"
-}: {
-  name: string;
+type PvProps = {
+  label: string;
   pv: string;
   pollInterval?: number;
   sigFigs?: number;
   dType?: string;
-}) => {
-  const { loading, error, data } = useQuery(getPvQuery(pv, dType),{pollInterval: pollInterval});
+};
+type PvComponentProps = PvProps & { render: PvItemComponent };
+type PvItem = { label: string; value: any };
+type PvItemComponent = ({ label, value }: PvItem) => JSX.Element;
+
+const PvComponent = (props: PvComponentProps) => {
+  var dType = props.dType || "float";
+  var sigFigs = props.dType || 4;
+  const { loading, error, data } = useQuery(getPvQuery(props.pv, dType), {
+    pollInterval: props.pollInterval,
+  });
   const result = () => {
     if (loading) return "Loading...";
     if (error) return `Error! ${error.message}`;
     if (dType === "float") {
-        return data.getChannel.value.float.toFixed(sigFigs);
+      return data.getChannel.value.float.toFixed(sigFigs);
     } else {
-        return data.getChannel.value[dType]
+      return data.getChannel.value[dType];
     }
-    
   };
-  return (
-    <HStack spacing={2}>
-      <Box borderWidth={1} padding={1}>
-        <Text>{name}:</Text>
-      </Box>
-      <Box borderWidth={1} padding={1}>
-        <Text>{result()}</Text>
-      </Box>
-    </HStack>
-  );
+  return props.render({ label: props.label, value: result() });
+};
+
+export const PollingRoPvBox = (props: PvProps) => {
+  console.log(props.label);
+  return PvComponent({
+    ...props,
+    render: (props: PvItem) => (
+      <HStack spacing={2}>
+        <Box borderWidth={1} padding={1}>
+          <Text>{props.label}:</Text>
+        </Box>
+        <Box borderWidth={1} padding={1}>
+          <Text>{props.value}</Text>
+        </Box>
+      </HStack>
+    ),
+  });
+};
+
+export const OnOffPvButtons = (props: PvProps) => {
+  let colormap = (state: any) => state === "Closed" ? "red" : "green"
+  return PvComponent({
+    ...props,
+    render: (props: PvItem) => (
+      <HStack spacing={2}>
+        <Box borderWidth={1} padding={1}>
+          <Text>{props.label}:</Text>
+        </Box>
+        <Box borderWidth={1} padding={1}>
+          <Button color={colormap(props.value)}>{props.value}</Button>
+        </Box>
+      </HStack>
+    ),
+  });
 };
