0785a8fbbb69cb05fa55cf3a1c3a67348d26b621
David Perl <david.perl@diamond.ac.uk>
2024-08-12 08:14:48 +0100

    separate screens into module

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { ChakraProvider, Box, Text, VStack, Grid, theme, HStack, Button } from "@chakra-ui/react";
+import { ChakraProvider, Box, Text, VStack, Grid, theme, HStack } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-import { PollingRoPvBox } from "./components/pv";
+import { SCREENS, TabbedScreens } from "./screens/main";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -13,30 +12,7 @@ export const App = () => (
             <Text fontSize="4xl">I24 SSX</Text>
             <ColorModeSwitcher justifySelf="flex-end" />
           </HStack>
-          <Tabs>
-            <TabList>
-              <Tab>Detector stage</Tab>
-              <Tab>2</Tab>
-              <Tab>3</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <HStack spacing={2}>
-                  <VStack spacing={1}>
-                    <PollingRoPvBox name="Detector" pv="ME14E-MO-IOC-01:GP101" dType="string" />
-                    <PollingRoPvBox name="Detector stage position" pv="BL24I-EA-DET-01:Y" />
-                    <Button>Move stage!</Button>
-                  </VStack>
-                </HStack>
-              </TabPanel>
-              <TabPanel>
-                <p>two!</p>
-              </TabPanel>
-              <TabPanel>
-                <p>three!</p>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          <TabbedScreens screens={SCREENS} />
         </VStack>
       </Grid>
     </Box>
