14c99b991841a295593850cb9c87d61e4ad281d6
David Perl <david.perl@diamond.ac.uk>
2024-08-12 18:13:44 +0100

    buttons to increase and decrease PV

--- src/components/pv.tsx
+++ src/components/pv.tsx
@@ -1,49 +1,80 @@
 import * as React from "react";
 import { Box, Text, HStack, Button } from "@chakra-ui/react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 
-const getPvQuery = (pv: string, type: string) =>
-  gql(`
-  query {
-    getChannel(id: "${pv}") {
+const GET_PV_QUERY = gql`
+  query ReadPv($id: ID!) {
+    getChannel(id: $id) {
       value {
-        ${type}
+        string
       }
     }
   }
-`);
+`;
 
-type PvProps = {
-  label: string;
+const SET_PV_QUERY = gql`
+  mutation SetPv($ids: [ID!]!, $values: [String!]!) {
+    putChannels(ids: $ids, values: $values) {
+      value {
+        string
+      }
+    }
+  }
+`;
+
+export enum PvDTypes {
+  Float,
+  Int,
+  String,
+}
+
+type ReadPvProps = {
   pv: string;
+  label: string;
   pollInterval?: number;
   sigFigs?: number;
-  dType?: string;
+  dType?: PvDTypes;
 };
-type PvComponentProps = PvProps & { render: PvItemComponent };
+type ReadPvComponentProps = ReadPvProps & { render: PvItemComponent };
+
+type SetPvProps = {
+  action_label: string;
+  modifyNumber?: (current: number) => number;
+  set_pv?: string;
+} & ReadPvProps;
+
 type PvItem = { label: string; value: any };
 type PvItemComponent = ({ label, value }: PvItem) => JSX.Element;
 
-const PvComponent = (props: PvComponentProps) => {
-  var dType = props.dType || "float";
-  var sigFigs = props.dType || 4;
-  const { loading, error, data } = useQuery(getPvQuery(props.pv, dType), {
-    pollInterval: props.pollInterval,
+const parseResult = (value: string, dType: PvDTypes, sigFigs?: number) => {
+  switch (dType) {
+    case PvDTypes.Float: {
+      return sigFigs ? parseFloat(value).toFixed(sigFigs) : parseFloat(value);
+    }
+    case PvDTypes.Int: {
+      return parseInt(value);
+    }
+    case PvDTypes.String: {
+      return value;
+    }
+  }
+};
+
+const PvComponent = (props: ReadPvComponentProps) => {
+  const { loading, error, data } = useQuery(GET_PV_QUERY, {
+    variables: { id: props.pv },
+    pollInterval: props.pollInterval || 100,
   });
   const result = () => {
     if (loading) return "Loading...";
     if (error) return `Error! ${error.message}`;
-    if (dType === "float") {
-      return data.getChannel.value.float.toFixed(sigFigs);
-    } else {
-      return data.getChannel.value[dType];
-    }
+    let rawResult: string = data.getChannel.value.string;
+    return props.dType ? parseResult(rawResult, props.dType, props.sigFigs) : rawResult;
   };
   return props.render({ label: props.label, value: result() });
 };
 
-export const PollingRoPvBox = (props: PvProps) => {
-  console.log(props.label);
+export const PollingRoPvBox = (props: ReadPvProps) => {
   return PvComponent({
     ...props,
     render: (props: PvItem) => (
@@ -59,8 +90,8 @@ export const PollingRoPvBox = (props: PvProps) => {
   });
 };
 
-export const OnOffPvButtons = (props: PvProps & {colormap: (value: any) => string}) => {
-  let colormap = props.colormap
+export const OnOffPvButton = (props: ReadPvProps & { colormap: (value: any) => string }) => {
+  let colormap = props.colormap;
   return PvComponent({
     ...props,
     render: (props: PvItem) => (
@@ -74,4 +105,21 @@ export const OnOffPvButtons = (props: PvProps & {colormap: (value: any) => strin
       </HStack>
     ),
   });
+};
+
+
+// TODO: make this use a context for the current value
+export const SetPvButton = (props: SetPvProps) => {
+  let modifyNumber = props.modifyNumber || ((x) => x);
+  const reading = useQuery(GET_PV_QUERY, {
+    variables: { id: props.pv },
+    pollInterval: props.pollInterval || 100,
+  });
+  let current = parseFloat(reading.data.getChannel.value.string)
+  const [setPv, { loading, error, data }] = useMutation(SET_PV_QUERY, {
+    variables: { ids: [props.set_pv || props.pv], values: [modifyNumber(current).toString()] },
+  });
+  if (loading) return <Box>Loading...</Box>;
+  if (error) return <Box>{`Error! ${error}`}</Box>;
+  return <Button onClick={(e) => setPv()}>{props.action_label}</Button>;
 };
